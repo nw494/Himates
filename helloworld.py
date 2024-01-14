4d1f33ebfbf7ed45496d1adb7867cf3fee60549d
nw494 <nikkiwalker494@googlemail.com>
2024-01-14 18:52:59 +0000

    first commit

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask web application
+app = Flask(__name__)
+
+# Define a route for the root URL ("/") that displays "Hello, World!"
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+# Run the web app on a local development server
+if __name__ == "__main__":
+    app.run(debug=True)
